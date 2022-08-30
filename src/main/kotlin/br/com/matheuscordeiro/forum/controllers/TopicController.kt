24c9f7608058fd4e19a07276ed4ac169a44e7421
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2022-08-30 01:23:35 -0300

    Post topic

--- src/main/kotlin/br/com/matheuscordeiro/forum/controllers/TopicController.kt
+++ src/main/kotlin/br/com/matheuscordeiro/forum/controllers/TopicController.kt
@@ -4,6 +4,8 @@ import br.com.matheuscordeiro.forum.models.Topic
 import br.com.matheuscordeiro.forum.services.TopicService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -18,5 +20,10 @@ class TopicController(private val topicService: TopicService) {
     @GetMapping("/{id}")
     fun getById(@PathVariable id: Long): Topic {
         return topicService.findById(id)
+    }
+
+    @PostMapping
+    fun create(@RequestBody topic: Topic) {
+        topicService.insert(topic)
     }
 }
--- src/main/kotlin/br/com/matheuscordeiro/forum/services/impl/TopicServiceImpl.kt
+++ src/main/kotlin/br/com/matheuscordeiro/forum/services/impl/TopicServiceImpl.kt
@@ -8,33 +8,6 @@ import org.springframework.stereotype.Service
 
 @Service
 class TopicServiceImpl(private var topics: List<Topic>) : TopicService {
-    init {
-        val topic = Topic(
-            id = 1,
-            tittle = "Kotlin",
-            message = "kotlin",
-            course = Course(1, name = "Kotlin", category = "Programming"),
-            author = User(id = 1, name = "Matheus Cordeiro", email = "email@email.com")
-        )
-
-        val topic2 = Topic(
-            id = 2,
-            tittle = "Kotlin 2",
-            message = "kotlin 2",
-            course = Course(1, name = "Kotlin", category = "Programming"),
-            author = User(id = 1, name = "Matheus Cordeiro", email = "email@email.com")
-        )
-
-        val topic3 = Topic(
-            id = 3,
-            tittle = "Kotlin 3",
-            message = "kotlin 3",
-            course = Course(1, name = "Kotlin", category = "Programming"),
-            author = User(id = 1, name = "Matheus Cordeiro", email = "email@email.com")
-        )
-        topics = listOf(topic, topic2, topic3)
-    }
-
     override fun findList(): List<Topic> {
         return topics
     }
@@ -43,5 +16,9 @@ class TopicServiceImpl(private var topics: List<Topic>) : TopicService {
         return topics.first {
             it.id == id
         }
+    }
+
+    override fun insert(topic: Topic) {
+        topics.plus(topic)
     }
 }
